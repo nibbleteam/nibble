83718d2504240aaadb94de572b4768cebc3fab04
Aline Alves <felipe.oltavares@gmail.com>
2017-11-20 07:46:30 -0300

    Demo gráfico!

--- src/apps/shell/main.lua
+++ src/apps/shell/main.lua
@@ -1,11 +1,46 @@
+w, h = 320, 240
+x, y = w/2, h/2
+vx, vy = 1, 1
+color = 1
+
+directions = {
+	{0, 1},
+	{1, 0},
+	{1, 1},
+	{-1, 1},
+	{-1, -1},
+	{1, -1},
+	{-1, 0},
+	{0, -1},
+}
+
 function update()
+	-- Muda de direção
+	if math.random() > 0.9 then
+		local d = directions[1+math.floor(math.random()*8)]
+		vx, vy = d[1], d[2]
+	end
+
+	-- Muda de cor
+	if math.random() > 0.9 then
+		-- Tudo menos preto
+		color = math.floor(math.random()*16) % 15 + 1
+	end
+
+	-- Move
+	x = x+vx
+	y = y+vy
+
+	-- "Bate" nas bordas
+	if x <= 0 or x >= w then
+		vx = vx*-1
+	end
+	if y <= 0 or y >= h then
+		vy = vy*-1
+	end
 end
 
 function draw()
-	for x=0,320,2 do
-		for y=0,240,2 do
-			local byte = string.char(math.floor(math.random()*255))
-			kernel.write(y*320+x, byte);
-		end
-	end
+	-- Desenha
+	kernel.write(x+y*w, string.char(color))
 end
