6875929d69924a35763bd55e4080ec72dcf8c7e5
Felipe Tavares <felipe.oltavares@gmail.com>
2017-11-21 05:27:02 -0300

    Utilizando shaders e transferência de memória diretamente para GPU
    Hacks:
    * Funcionando apenas com 1 pixel
    * Formato de cor BRGA
    * Shaders padrão sem modificação

--- src/kernel/drivers/VideoMemory.cpp
+++ src/kernel/drivers/VideoMemory.cpp
@@ -1,4 +1,34 @@
+#include <SFML/OpenGL.hpp>
 #include <kernel/drivers/VideoMemory.hpp>
+#include <iostream>
+
+const string VideoMemory::writeShaderVertex = R"(
+// Shader padrão do SFML sem alterações
+void main()
+{
+    // transform the vertex position
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+
+    // transform the texture coordinates
+    gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
+
+    // forward the vertex color
+    gl_FrontColor = gl_Color;
+}
+)";
+
+const string VideoMemory::writeShaderFragment = R"(
+uniform sampler2D texture;
+
+void main()
+{
+    // lookup the pixel in the texture
+    vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);
+
+    // multiply it by the color
+    gl_FragColor = gl_Color * pixel;
+}
+)";
 
 VideoMemory::VideoMemory(sf::RenderWindow &window, const unsigned int w, const unsigned int h, const uint64_t addr):
 	w(w), h(h), address(addr), length(w*h), window(window), dirty(false) {
@@ -9,6 +39,13 @@ VideoMemory::VideoMemory(sf::RenderWindow &window, const unsigned int w, const u
 	rwTex = sf::Texture();
 	rwTex.create(w, h);
 	cpuSpr = sf::Sprite(rwTex);
+
+    // Tenta carregar o shader, em caso de erro termina
+    // uma vez que não será possível mostrar nada
+    if (!writeShader.loadFromMemory(writeShaderVertex, writeShaderFragment)) {
+        cout << "video " << "error loading write shader" << endl;
+        exit(1);
+    }
 }
 
 VideoMemory::~VideoMemory() {
@@ -16,48 +53,25 @@ VideoMemory::~VideoMemory() {
 }
 
 void VideoMemory::draw() {
-	rwTex.update(img);
-	renderTex.draw(cpuSpr);
+	renderTex.draw(cpuSpr, &writeShader);
 
 	window.draw(gpuSpr);
 }
 
 uint64_t VideoMemory::write(const uint64_t p, const uint8_t* data, const uint64_t size) {
-	static sf::Color pallete[] = {
-		sf::Color(0, 0, 0),
-		sf::Color(255, 255, 255),
-		sf::Color(255, 200, 255),
-		sf::Color(30, 128, 43),
-		sf::Color(24, 25, 32),
-		sf::Color(8, 145, 223),
-		sf::Color(211, 231, 45),
-		sf::Color(53, 23, 23),
-		sf::Color(64, 255, 255),
-		sf::Color(223, 232, 245),
-		sf::Color(255, 255, 255),
-		sf::Color(252, 25, 225),
-		sf::Color(124, 255, 255),
-		sf::Color(223, 123, 255),
-		sf::Color(12, 54, 128),
-		sf::Color(12, 54, 3),
-	};
-
 	unsigned int x, y;
 
 	x = (unsigned int)p % w;
 	y = (unsigned int)p / w;
+
+    if (x > w || y > h) {
+        return 0;
+    }
 	
-	for (int i=0;i<size;i++) {
-		auto color = data[i]%16;
+    sf::Texture::bind(&rwTex);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, 1, 1, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, data);
 
-		img.setPixel(x, h-y-1, pallete[color]);
-		x++;
-		if (x >= w) {
-			x = 0; y++;
-		}
-	}
-
-	return 0;
+	return size;
 }
 
 uint64_t VideoMemory::read(const uint64_t, uint8_t*, const uint64_t) {
