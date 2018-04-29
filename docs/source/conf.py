0eeb1689295b3758ad8231f51468e8364d34d83d
Felipe Tavares <--global>
2018-04-29 17:13:24 -0300

    Removidas todas as opções de tema

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,19 +78,6 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    'canonical_url': '',
-    'analytics_id': '',
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'titles_only': False
-}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
