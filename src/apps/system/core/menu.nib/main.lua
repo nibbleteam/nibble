163bd10ff6459df312a77ab0fe97f467e16a933d
Felipe Tavares <felipe.oltavares@gmail.com>
2018-11-19 07:55:20 -0300

    Menu redesign

--- src/apps/system/core/menu.nib/main.lua
+++ src/apps/system/core/menu.nib/main.lua
@@ -1,8 +1,7 @@
 local prev_screen = ""
-local quit = false
 
-local x=0
-local y=0
+local x=320/2-160/2
+local y=240/2-80/2
 local vx=2
 local vy=1
 local dt=1
@@ -14,49 +13,31 @@ end
 function draw()
     --kernel.write(544, prev_screen)
 
-    if not quit then
-        rectf(x, y, 150, 70, 2)
+    rectf(x-2, y-2, 164, 84, 3)
+    rectf(x, y, 160, 80, 2)
 
-        print("FPS: "..tostring(math.floor(1/dt+0.5)), x+94, y)
-        print("Menu", x, y)
-        print("v1", x, y+10)
-        print("ENV PID: "..kernel.getenv("pid"), x, y+20)
-        print("ENV APP.PID: "..kernel.getenv("app.pid"), x, y+30)
+    local x = x + 4
+    local y = y + 4
 
-        print("\08 go back", x, y+50)
-        print("\09 close cartridge", x, y+60)
-    end
+    print("FPS: "..tostring(math.floor(1/dt+0.5)), x+94, y)
+    print("Menu", x, y)
+    print("v1", x, y+10)
+    print("ENV PID: "..kernel.getenv("pid"), x, y+20)
+    print("ENV APP.PID: "..kernel.getenv("app.pid"), x, y+30)
+
+    print("\08 go back", x, y+50)
+    print("\09 close cartridge", x, y+60)
 end
 
 function update(delta)
     dt = delta
-    if btp(RED) and kernel.getenv("app.pid") ~= "1" then
-        kernel.read(154154, 32)
+    if btp(RED) and kernel.getenv("app.pid") ~= "2" then
         kernel.kill(tonumber(kernel.getenv("app.pid")))
         kernel.kill(0)
-        quit = true
     end
 
-    if btp(BLUE) or (btp(RED) and kernel.getenv("app.pid") == "1") then
-        kernel.read(154154, 32)
+    if btp(BLUE) or (btp(RED) and kernel.getenv("app.pid") == "2") then
         kernel.setenv("menu.entry", "back")
         kernel.kill(0)
-        quit = true
-    end
-
-    x = x+vx
-    y = y+vy
-
-    if x > 320-150 then
-        vx = vx*-1
-    end
-    if x < 0 then
-        vx = vx*-1
-    end
-    if y > 240-70 then
-        vy = vy*-1
-    end
-    if y < 0 then
-        vy = vy*-1
     end
 end
