f8494f2164ce962b3fa07ee2217c29396bd7f663
Felipe Tavares <felipe.oltavares@gmail.com>
2018-11-22 12:11:38 -0300

    Removed debug code from init and shell

--- src/apps/system/core/init.nib/main.lua
+++ src/apps/system/core/init.nib/main.lua
@@ -1,22 +1,4 @@
 function init()
-    local x = 1
-    local y = {
-        y = 1
-    }
-    local z = {
-        z = {
-            z = 1
-        }
-    }
-
-    x += 1
-    y.y += 1
-    z.z.z += 1
-
-    dprint(x)
-    dprint(y.y)
-    dprint(z.z.z)
-
     kernel.exec("apps/system/core/shell.nib", {})
     kernel.kill(0)
 end
