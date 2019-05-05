3d6a9a132f20ea3e66fb18df331d4210aa97da22
Felipe Tavares <felipe.oltavares@gmail.com>
2019-05-05 08:31:36 -0300

    Exporting symbols

--- include/kernel/Kernel.hpp
+++ include/kernel/Kernel.hpp
@@ -22,6 +22,12 @@
 #include <devices/Controller.hpp>
 #include <devices/MidiController.hpp>
 
+#ifdef WIN32
+#define API __declspec(dllexport)
+#else
+#define API 
+#endif
+
 using namespace std;
 
 // Simula o hardware do console e um kernel.
@@ -74,42 +80,42 @@ public:
 };
 
 extern "C" {
-    typedef struct LuaString {
+    API typedef struct LuaString {
         char* ptr;
         size_t len;
     } LuaString;
 
     // Memória
-    size_t kernel_api_read(char*, const size_t, const size_t);
-    size_t kernel_api_write(const size_t, const size_t, const char*);
+    API size_t kernel_api_read(char*, const size_t, const size_t);
+    API size_t kernel_api_write(const size_t, const size_t, const char*);
 
     // Spritesheets
-    void kernel_api_load_spritesheet(const char*, size_t*, int*, int*);
-    void kernel_api_use_spritesheet(const size_t, const int, const int);
+    API void kernel_api_load_spritesheet(const char*, size_t*, int*, int*);
+    API void kernel_api_use_spritesheet(const size_t, const int, const int);
 
     // Arquivos
-    LuaString* api_list_files(const char*, size_t*, int*);
+    API LuaString* api_list_files(const char*, size_t*, int*);
 
     // Desenho
-    void gpu_api_line(int16_t, int16_t, int16_t, int16_t, uint8_t);
-    void gpu_api_rect(int16_t, int16_t, int16_t, int16_t, uint8_t);
-    void gpu_api_tri(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint8_t);
-    void gpu_api_quad(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint8_t);
-    void gpu_api_circle(int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_line(int16_t, int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_rect(int16_t, int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_tri(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_quad(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_circle(int16_t, int16_t, int16_t, uint8_t);
 
-    void gpu_api_rect_fill(int16_t, int16_t, int16_t, int16_t, uint8_t);
-    void gpu_api_tri_fill(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint8_t);
-    void gpu_api_quad_fill(int16_t, int16_t,
+    API void gpu_api_rect_fill(int16_t, int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_tri_fill(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_quad_fill(int16_t, int16_t,
                            int16_t, int16_t,
                            int16_t, int16_t,
                            int16_t, int16_t,
                            uint8_t);
-    void gpu_api_circle_fill(int16_t, int16_t, int16_t, uint8_t);
-    void gpu_api_sprite(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint8_t);
-    void gpu_api_clip(int16_t, int16_t, int16_t, int16_t);
-    void gpu_api_clear(uint8_t);
-    int gpu_start_capturing(const char*);
-    int gpu_stop_capturing();
+    API void gpu_api_circle_fill(int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_sprite(int16_t, int16_t, int16_t, int16_t, int16_t, int16_t, uint8_t);
+    API void gpu_api_clip(int16_t, int16_t, int16_t, int16_t);
+    API void gpu_api_clear(uint8_t);
+    API int gpu_start_capturing(const char*);
+    API int gpu_stop_capturing();
 
 #include <cstdlib>
 }
