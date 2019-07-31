4d2c2293b4fa6e1df9350df301c3ecd464bfdc05
Felipe Tavares <--global>
2019-07-30 23:29:08 -0300

    Preparing for the first release
    Removed demos, old apps, WIP apps, old README and meson leftovers

--- src/apps/music.nib/elements/status.lua
+++
@@ -1,9 +0,0 @@
-local d = require('nibui.NOM').dynamic
-
-return {
-    x = d'left', y = d'-'(d'bottom', 12),
-    w = d'^' 'w', h = 12,
-    background = 6,
-    text_align = 'left',
-    padding_left = 2,
-    content = '...', }
