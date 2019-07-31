c7123a84fc6f7f094139bcdda43b4eb39cb84ac5
Felipe Tavares <--global>
2019-07-30 23:37:52 -0300

    lua.nib: using the new niblib API

--- src/apps/lua.nib/main.lua
+++ src/apps/lua.nib/main.lua
@@ -6,32 +6,29 @@ local input = ''
 local prompt_color = 11
 
 function shprint(str, bg)
-    kernel.send(tty, { print = str, background = bg})
+    send_message(env.tty, { print = str, background = bg})
 end
 
 function init()
-    tty = tonumber(kernel.getenv('tty'))
-
-    kernel.send(tty, { subscribe = tonumber(kernel.getenv('pid')), name = 'lua.nib' })
+    send_message(env.shell, { tty = true })
+    send_message(env.tty, { subscribe = env.pid, name = 'lua.nib' })
 
     shprint(_VERSION..'\n', 6)
     shprint(prompt, prompt_color)
     shprint(' ')
-
-    start_recording('lua.gif')
 end
 
 local more_input = false
 
 function update()
-    local message = kernel.receive()
+    local message = receive_message()
 
     if message then
         if message.input then
             local fn, err = load('return (' .. input..message.input .. ')')
 
             if message.input == 'exit' or message.input == 'quit' then
-                kernel.kill(0)
+                stop_app(0)
                 return
             end
 
