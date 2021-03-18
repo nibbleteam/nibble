f45e2c6f65ab63b9982413df2b8c3b8500930880
Felipe Tavares <felipe.oltavares@gmail.com>
2021-03-18 09:41:42 -0300

    Fix in DirectX headers.

--- include/devices/GPU/directx.hpp
+++ include/devices/GPU/directx.hpp
@@ -6,5 +6,6 @@
 
 SDL_Renderer* create_directx_renderer(SDL_Window*);
 IDirect3DPixelShader9 **create_directx_shader(SDL_Renderer*);
+void use_hlsl_shader(SDL_Renderer*, IDirect3DPixelShader9 **);
 
 #endif // _GPU_DIRECTX_H_
