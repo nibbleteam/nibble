ca21a7f60d97cbe4698e153aff0f30fb6cf6957e
Felipe Tavares <felipe.oltavares@gmail.com>
2021-03-20 23:31:56 -0300

    Test if we can disable the shader safely.

--- src/devices/GPU/directx.cpp
+++ src/devices/GPU/directx.cpp
@@ -81,6 +81,7 @@ void use_hlsl_shader(SDL_Renderer* renderer, IDirect3DPixelShader9 *shader) {
     IDirect3DDevice9* device = ((D3D_RenderData*)renderer->driverdata)->device;
 
     assert(D3D_OK == IDirect3DDevice9_SetPixelShader(device, shader));
+    assert(D3D_OK == IDirect3DDevice9_SetPixelShader(device, nullptr));
 }
 
 #endif
